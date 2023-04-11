b7507cec32ef5960118a8851dd18fe3eb7299c16
Shijin-Varghese <108483342+Shijin-Varghese@users.noreply.github.com>
2023-04-12 00:31:09 +0530

    d1

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -2,11 +2,11 @@ package com.driver;
 
 public class RWOnly {
     private String name;
-    void setName(String s){
+    public void setName(String s){
         name=s;
 //        System.out.println(name);
     }
-    String getName(){
+    public String getName(){
        return name;
     }
 }
