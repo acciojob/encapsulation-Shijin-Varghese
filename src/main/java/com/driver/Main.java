02a377c01508f74d58537dd06f376e0a64ec0d61
Shijin-Varghese <108483342+Shijin-Varghese@users.noreply.github.com>
2023-04-12 00:28:56 +0530

    doubt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,6 +3,7 @@ package com.driver;
 public class Main {
   public static void main(String args[]){
    RWOnly rw=new RWOnly();
-
+  rw.setName("Shijin");
+      System.out.println(rw.getName());
   }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+    void setName(String s){
+        name=s;
+//        System.out.println(name);
+    }
+    String getName(){
+       return name;
+    }
 }
